7f2f6e1ac84ada05595bf3fba0516bcaa161ac9c
PkMsk <pk693@icloud.com>
2023-11-02 23:47:31 +0300

    Исправляет замечания согласно ревью

--- source/script.js
+++ source/script.js
@@ -2,18 +2,19 @@ let navList = document.querySelector('.navigation__list');
 let navToggle = document.querySelector('.navigation__toggle');
 let navMain = document.querySelector('.navigation');
 
+navToggle.classList.remove('navigation__toggle--nojs');
 navMain.classList.remove('navigation--nojs');
 
-navToggle.addEventListener ('click', function () {
-if (navToggle.classList.contains('navigation__toggle--closed') && navList.classList.contains('navigation__list--closed')) {
-navToggle.classList.remove('navigation__toggle--closed');
-navList.classList.remove('navigation__list--closed');
-navToggle.classList.add('navigation__toggle--opened');
-navList.classList.add('navigation__list--opened');
-} else {
-navToggle.classList.add('navigation__toggle--closed');
-navList.classList.add('navigation__list--closed');
-navToggle.classList.remove('navigation__toggle--opened');
-navList.classList.remove('navigation__list--opened');
-}
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('navigation__toggle--closed') && navList.classList.contains('navigation__list--closed')) {
+    navToggle.classList.remove('navigation__toggle--closed');
+    navList.classList.remove('navigation__list--closed');
+    navToggle.classList.add('navigation__toggle--opened');
+    navList.classList.add('navigation__list--opened');
+  } else {
+    navToggle.classList.add('navigation__toggle--closed');
+    navList.classList.add('navigation__list--closed');
+    navToggle.classList.remove('navigation__toggle--opened');
+    navList.classList.remove('navigation__list--opened');
+  }
 })
