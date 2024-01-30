62b45747b15ced54714e5f2a21d6fccdb243f757
Keks <keks@htmlacademy.ru>
2024-01-31 00:12:50 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/app.js
@@ -0,0 +1,21 @@
+let headerWrapper = document.querySelector('.header__wrapper');
+let headerToggle = document.querySelector('.header__toggle');
+let navList = document.querySelector('.navigation__list');
+let navMain = document.querySelector('.navigation');
+
+headerWrapper.classList.remove('header__wrapper--nojs');
+navMain.classList.remove('navigation--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (headerToggle.classList.contains('header__toggle--closed') && navList.classList.contains('navigation__list--closed')) {
+    headerToggle.classList.remove('header__toggle--closed');
+    navList.classList.remove('navigation__list--closed');
+    headerToggle.classList.add('header__toggle--opened');
+    navList.classList.add('navigation__list--opened');
+  } else {
+    headerToggle.classList.add('header__toggle--closed');
+    navList.classList.add('navigation__list--closed');
+    headerToggle.classList.remove('header__toggle--opened');
+    navList.classList.remove('navigation__list--opened');
+  }
+})
